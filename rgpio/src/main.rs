6a661910d2385125d326a0532380b2c0a223f96a
Manfred Michaelis <mm@michm.de>
2024-10-02 23:21:42 +0200

    doc: provided some comments

--- rgpio/src/main.rs
+++ rgpio/src/main.rs
@@ -1,34 +1,51 @@
 use std::env;
 use std::{thread, time};
+use std::error::Error;
 use rgpiolib::gpio;
 
-fn main() {
+fn main() -> Result<(), dyn Error> {
+    // Collect command-line arguments into a vector of strings
     let args: Vec<String> = env::args().collect();
 
+    // Check if no arguments are provided
     if args.len() == 1 {
         print!("usage: rgpio ...[num]");
     } else {
+        // Iterate over the provided arguments
         for i in 1..args.len() {
-            let gpio: i32;
-
-            match args[i].parse() {
+            // Parse the argument to an integer
+            let gpio_num = match args[i].parse() {
                 Err(why) => panic!("an error has occurred: {}", why),
-                Ok(num) => gpio = num,
+                Ok(num) => num,
+            };
+
+            // Export the GPIO pin and set its direction to output
+            if (gpio::export(gpio_num).is_ok()) {
+                gpio::set_direction(gpio_num, gpio::Directions::Output)?;
+                gpio::write(gpio_num, true)?;
+
+                // Read the GPIO pin value and print it
+                let mut val = gpio::read(gpio_num)?;
+                println!("{}", val);
+
+                // Sleep for 500 milliseconds
+                thread::sleep(time::Duration::from_millis(500));
+
+                // Write false to the GPIO pin and sleep again
+                gpio::write(gpio_num, false)?;
+                thread::sleep(time::Duration::from_millis(500));
+
+                // Read the GPIO pin value again and print it
+                val = gpio::read(gpio_num)?;
+                println!("{}", val);
+
+                // Unexport the GPIO pin
+                gpio::unexport(gpio_num)?;
+            } else {
+                println!("failed to export gpio {}", gpio_num);
             }
+        }
+    }
 
-            gpio::export(gpio);
-            gpio::set_direction(gpio, gpio::Directions::Output);
-            gpio::write(gpio, true);
-            let mut val = gpio::read(gpio);
-            println!("{}", val);
-
-            thread::sleep(time::Duration::from_millis(500));
-
-            gpio::write(gpio, false);
-            thread::sleep(time::Duration::from_millis(500));
-            val = gpio::read(gpio);
-            println!("{}", val);
-            gpio::unexport(gpio);
-        }   
-    }     
+    Ok(())
 }
